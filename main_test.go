4636e4473043a29620b777a67277128a962537b5
toddineagan <47012256+toddineagan@users.noreply.github.com>
2024-05-17 15:14:54 -0500

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -14,7 +14,7 @@ func TestHandler(t *testing.T) {
 		Headers: map[string]string{
 			"Content-Type": "text/html",
 		},
-		Body: "Congratulations",
+		Body: "Hey",
 	}
 
 	response, err := Handler(request)
